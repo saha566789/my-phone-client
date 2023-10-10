ba35122fbeccecf9be40ced0d692b8df3ed4fb4e
saha566789 <tomasaha3456@gmail.com>
2023-10-10 17:11:47 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+var cors = require('cors')
+const phones =require('./phone.json')
+const app = express();
+const port = 5000;
+
+app.use(cors())
+
+app.get('/',(req,res)=>{
+ res.send('my phone soon')
+})
+
+app.get('/phones',(req,res)=>{
+    res.send(phones)
+})
+
+app.get('/phones/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log('i need data for id: ', id);
+    const phone = phones.find(phone => phone.id === id) || {};
+    res.send(phone);
+})
+
+app.listen(port,()=>{
+    console.log(`my phone server:${port}`)
+})
